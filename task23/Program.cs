56f19d2fca04e83d9c57276a5eec60c974787349
IrinaKisialiova <irisha252008@rambler.ru>
2023-05-02 21:11:00 +0300

    написали программу по task23

---
+++ task23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int n = Math.Abs(Promt("Введите число N: "));
+NumTable(n);
+
+int Promt(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    int numN = Convert.ToInt32(value);
+    return numN;
+}
+
+void NumTable(int n)
+{
+    int currentNum = 1;
+    int sqr = 1;
+    while (currentNum <= n)
+    {
+        sqr = currentNum * currentNum * currentNum;
+        Console.WriteLine(sqr);
+        currentNum++;
+    }
+}
